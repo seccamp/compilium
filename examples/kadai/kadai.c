d411c50cd2efc85bdc592b7998eb6bbc5954dffe
iwannatto <iwannatto@gmail.com>
2020-11-14 20:42:57 +0900

    Add space & Make it enable to take input > INT_MAX

--- examples/kadai/kadai.c
+++ examples/kadai/kadai.c
@@ -19,25 +19,25 @@ Example: 910
 // "._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.",
 // "|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|",
 // "|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|",
+
+#include <stdio.h>
+
 char num2str[3][58]= {
   "._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.",
   "|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|",
   "|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|"
 };
 
-#include <stdio.h>
-void to_lcd(int n){
-    char buf[12];
-    snprintf(buf, 12, "%d", n);
+void to_lcd(char *buf){
     for (int j = 0; j < 3; ++j) {
         for (int i = 0; *(buf + i) != 0; ++i) {
             int v = buf[i]-'0'; // when buf[i] : '5' v = 5
-            printf("%.3s", &(num2str[j][6*v]));
+            printf("%.3s ", &(num2str[j][6*v]));
         }
         printf("\n");
     }
 }
 
-int main() {
-    to_lcd(910);
+int main(int argc, char **argv) {
+    to_lcd(argv[1]);
 }
