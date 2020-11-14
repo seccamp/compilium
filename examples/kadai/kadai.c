ed3a8e263a76664bda1369d9a6a38e49b6fa834e
TumoiYorozu <TumoiYorozu@gmail.com>
2020-11-14 20:22:27 +0900

    wip

--- examples/kadai/kadai.c
+++ examples/kadai/kadai.c
@@ -16,15 +16,20 @@ Example: 910
 ..| ..| |_|
 */
 
+// "._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.",
+// "|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|",
+// "|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|",
+
 #include <stdio.h>
 void to_lcd(int n){
     char buf[12];
     snprintf(buf, 12, "%d", n);
     char lcd[3][30];
-    for (char* p = buf; *p != 0; p++){
+    char line1[];
+    for (int i = 0; *(buf + i) != 0; ++i) {
+        int v = buf[i]-'0'; // when buf[i] : '5' v = 5
 
     }
-
 }
 
 int main() {
